6e286aaf257382a2db83b3c0c0e42a9ea4da2a3d
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2019-09-09 13:27:25 -0500

    se obtiene la referencia de la bd

--- src/db/DBController.ts
+++ src/db/DBController.ts
@@ -17,7 +17,8 @@ const getIDCCobro = async (req: any) => {
                 'Cobro'::text AS descripcion,
                 extract(epoch from mc.fecha_hora_solicitud)*1000 AS fecha,
                 mc.numero_celular_vendedor::text AS vnc,
-                mc.digito_verificador_vendedor::text AS vdv
+                mc.digito_verificador_vendedor::text AS vdv,
+                mc.referencia_numerica as r
             FROM
                 codi_mc_generados AS mc
             WHERE
@@ -53,6 +54,7 @@ const getIDCPago = async (req: any) => {
                 oper.digito_verificador_comprador::text AS cdv,
                 oper.numero_celular_vendedor::text AS vnc,
                 oper.digito_verificador_vendedor::text AS vdv
+                oper.referencia_numerica as r
             FROM codi_operaciones AS oper
             INNER JOIN codi_tipo_operacion AS tOper ON tOper.id_tipo_operacion = oper.id_tipo_operacion
             WHERE
