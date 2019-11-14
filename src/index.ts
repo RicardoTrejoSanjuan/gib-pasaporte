005ff6054678d3d2c81e598f55eb737c2ac65a27
Luis E. González <luisegr149@gmail.com>
2019-11-13 18:45:38 -0600

    Deploy YAML

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import app from './app';
 import { Logger } from 'logger-colors';
 import packageJson from '../package.json';
 import {art} from 'commons';
+require ('@instana/collector')({
+    agentHost: process.env.INSTANA_AGENT_HOST,
+    level: 'error',
+});
+
 const logger = new Logger();
 const port: number = Number(process.env.PORT) || 8080;
+
 
 app.listen(port, () => {
     logger.success(art);
